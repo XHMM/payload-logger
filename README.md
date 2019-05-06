7942ddb0c5d37b16f462ca2ac47729568aedd54c
xhmm <super.xhmm@gmail.com>
2019-05-06 22:12:21 +0800

    fix error when no body parsed to request

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 This is a simple express middleware to log `request.query` and `request.body` to console for debugging
 
 ### Usage
-1. `npm i payload-logger`
-2. `import payloadLogger from 'payload-logger''` or `const payloadLogger = require('payload-logger'').default`
-3. `app.use(payloadLogger)`
+
+1. `npm i body-parser payload-logger`
+2. 
+```typescript
+import payloadLogger from 'payload-logger'
+import bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(payloadLogger)
+```
--- index.ts
+++ index.ts
@@ -30,10 +30,12 @@ export default function payloadLogger(
   if (Object.getOwnPropertyNames(req.query).length) {
     logger.query(req.path, req.query);
   }
-  if (Object.getOwnPropertyNames(req.body).length) {
-    // 目前暂不清楚：当content-type为application/x-www-form-urlencoded时，直接打印req.body会出现[Object: null prototype] {body content}
-    // 网上查了下，没说具体啥原因，只能暂用JSON.xx即可解决
-    logger.body(req.path, JSON.parse(JSON.stringify(req.body)));
+  if (req.body) {
+    if (Object.getOwnPropertyNames(req.body).length) {
+      // 目前暂不清楚：当content-type为application/x-www-form-urlencoded时，直接打印req.body会出现[Object: null prototype] {body content}
+      // 网上查了下，没说具体啥原因，只能暂用JSON.xx即可解决
+      logger.body(req.path, JSON.parse(JSON.stringify(req.body)));
+    }
   }
   next();
 }
